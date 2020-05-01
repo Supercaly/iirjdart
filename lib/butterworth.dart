1532e745c7e2df4368ccedb88a06f3eec421b3ef
supercaly <djonline96@gmail.com>
2020-05-01 11:09:54 +0200

    Add butterworth.dart, chebyshev_1.dart and chebyshev_2.dart filters

---
+++ lib/butterworth.dart
@@ -0,0 +1,127 @@
+
+import 'dart:math' as math;
+
+import 'package:complex/complex.dart';
+import 'package:iirjdart/band_pass_transform.dart';
+import 'package:iirjdart/band_stop_transform.dart';
+import 'package:iirjdart/cascade.dart';
+import 'package:iirjdart/direct_form.dart';
+import 'package:iirjdart/high_pass_transform.dart';
+import 'package:iirjdart/layout_base.dart';
+import 'package:iirjdart/low_pass_transform.dart';
+
+class _AnalogLowPass extends LayoutBase {
+  int _nPoles;
+
+  _AnalogLowPass(int nPoles): super(nPoles) {
+    _nPoles = nPoles;
+    setNormal(0, 1);
+  }
+
+  void design() {
+    reset();
+    double n2 = (2 * _nPoles).toDouble();
+    int pairs = _nPoles ~/ 2;
+    for (int i = 0; i < pairs; ++i) {
+      Complex c = Complex.polar(1.0, math.pi / 2.0 + (2 * i + 1) * math.pi / n2);
+      addPoleZeroConjugatePairs(c, Complex.INFINITY);
+    }
+
+    if ((_nPoles & 1) == 1)
+      add( Complex(-1), Complex.INFINITY);
+  }
+}
+
+/// Create a Butterworth filter.
+///
+/// Example:
+/// ```dart
+///   Butterworth butterworth = new Butterworth();
+///   butterworth.bandPass(2,250,50,5);
+/// ```
+class Butterworth extends Cascade {
+
+  /// Butterworth Low-pass filter.
+  ///
+  /// Params:
+  /// * order - The order of the filter
+  /// * sampleRate - The sampling rate of the system
+  /// * cutoffFrequency - the cutoff frequency
+  /// * directFormType - The filter topology. Default direct_form_II
+  void lowPass(
+    int order,
+    double sampleRate,
+    double cutoffFrequency,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design();
+    LayoutBase digitalProto =  LayoutBase(order);
+    LowPassTransform(cutoffFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+
+  /// Butterworth High-pass filter.
+  ///
+  /// Params:
+  /// * order - Filter order (ideally only even orders)
+  /// * sampleRate - Sampling rate of the system
+  /// * cutoffFrequency - Cutoff of the system
+  /// * directFormType - The filter topology. Default direct_form_II
+  void highPass(
+    int order,
+    double sampleRate,
+    double cutoffFrequency,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design();
+    LayoutBase digitalProto =  LayoutBase(order);
+    HighPassTransform(cutoffFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+
+  /// Butterworth Bandstop filter.
+  ///
+  /// Params:
+  /// * order - Filter order (actual order is twice)
+  /// * sampleRate - Sampling rate of the system
+  /// * centerFrequency - Center frequency
+  /// * widthFrequency - Width of the notch
+  /// * directFormType - The filter topology. Default direct_form_II
+  void bandStop(
+    int order,
+    double sampleRate,
+    double centerFrequency,
+    double widthFrequency,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design();
+    LayoutBase digitalProto =  LayoutBase(order * 2);
+    BandStopTransform(centerFrequency / sampleRate, widthFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+
+  /// Butterworth Bandpass filter.
+  ///
+  /// Params:
+  /// * order - Filter order
+  /// * sampleRate - Sampling rate
+  /// * centerFrequency - Center frequency
+  /// * widthFrequency - Width of the notch
+  /// * directFormType - The filter topology. Default direct_form_II
+  void bandPass(
+    int order,
+    double sampleRate,
+    double centerFrequency,
+    double widthFrequency,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design();
+    LayoutBase digitalProto =  LayoutBase(order * 2);
+    BandPassTransform(centerFrequency / sampleRate, widthFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+}
---
+++ lib/chebyshev_2.dart
@@ -0,0 +1,148 @@
+
+import 'dart:math' as math;
+
+import 'package:complex/complex.dart';
+import 'package:complex/fastmath.dart';
+import 'package:iirjdart/band_pass_transform.dart';
+import 'package:iirjdart/band_stop_transform.dart';
+import 'package:iirjdart/cascade.dart';
+import 'package:iirjdart/direct_form.dart';
+import 'package:iirjdart/high_pass_transform.dart';
+import 'package:iirjdart/layout_base.dart';
+import 'package:iirjdart/low_pass_transform.dart';
+import 'package:iirjdart/math_supplement.dart';
+
+class _AnalogLowPass extends LayoutBase {
+  int nPoles;
+
+  _AnalogLowPass(int _nPoles):
+    nPoles = _nPoles,
+    super(_nPoles);
+
+  void design(double stopBandDb) {
+    reset();
+
+    double eps = math.sqrt(1.0 / (math.exp(stopBandDb * 0.1 * MathSupplement.doubleLn10) - 1));
+    double v0 = MathSupplement.asinh(1 / eps) / nPoles;
+    double sinhV0 = -sinh(v0);
+    double coshV0 = cosh(v0);
+    double fn = math.pi / (2 * nPoles);
+
+    int k = 1;
+    for (int i = nPoles ~/ 2; --i >= 0; k += 2) {
+      double a = sinhV0 * math.cos((k - nPoles) * fn);
+      double b = coshV0 * math.sin((k - nPoles) * fn);
+      double d2 = a * a + b * b;
+      double im = 1 / math.cos(k * fn);
+      Complex pole = Complex(a / d2, b / d2);
+      Complex zero = Complex(0.0, im);
+      addPoleZeroConjugatePairs(pole, zero);
+    }
+
+    if ((nPoles & 1) == 1) {
+      add( Complex(1 / sinhV0),  Complex(double.infinity));
+    }
+    setNormal(0, 1);
+  }
+}
+
+/// Create a ChebyshevII filter.
+///
+/// Example:
+/// ```dart
+///   ChebyshevII chebyshev = new ChebyshevII();
+///   chebyshev.bandPass(2,250,50,5);
+/// ```
+class ChebyshevII extends Cascade {
+  /// ChebyshevI Lowpass filter with custom topology
+  ///
+  /// Param:
+  /// * order - The order of the filter
+  /// * sampleRate - The sampling rate of the system
+  /// * cutoffFrequency - The cutoff frequency
+  /// * rippleDb - pass-band ripple in decibel sensible value: 1dB
+  /// * directFormType -The filter topology. Default direct_form_II
+  void lowPass(
+    int order,
+    double sampleRate,
+    double cutoffFrequency,
+    double rippleDb,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto = _AnalogLowPass(order);
+    analogProto.design(rippleDb);
+    LayoutBase digitalProto = LayoutBase(order);
+    LowPassTransform(cutoffFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+
+   /// ChebyshevI Lowpass filter and custom filter topology
+   ///
+  /// Param:
+   /// * order - The order of the filter
+   /// * sampleRate - The sampling rate of the system
+   /// * cutoffFrequency - The cutoff frequency
+   /// * rippleDb - pass-band ripple in decibel sensible value: 1dB
+   /// * directFormType - The filter topology. Default direct_form_II
+  void highPass(
+    int order,
+    double sampleRate,
+    double cutoffFrequency,
+    double rippleDb,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design(rippleDb);
+    LayoutBase digitalProto =  LayoutBase(order);
+    HighPassTransform(cutoffFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+
+  /// Bandstop filter with custom topology
+  ///
+  /// Param:
+  /// * order - Filter order (actual order is twice)
+  /// * sampleRate - Sampling rate of the system
+  /// * centerFrequency - Center frequency
+  /// * widthFrequency - Width of the notch
+  /// * rippleDb - pass-band ripple in decibel sensible value: 1dB
+  /// * directFormType - The filter topology. Default direct_form_II
+  void bandStop(
+    int order,
+    double sampleRate,
+    double centerFrequency,
+    double widthFrequency,
+    double rippleDb,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design(rippleDb);
+    LayoutBase digitalProto =  LayoutBase(order * 2);
+    BandStopTransform(centerFrequency / sampleRate, widthFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+
+  /// Bandpass filter with custom topology
+  ///
+  /// Param:
+  /// * order - Filter order
+  /// * sampleRate -  Sampling rate
+  /// * centerFrequency - Center frequency
+  /// * widthFrequency - Width of the notch
+  /// * rippleDb - pass-band ripple in decibel sensible value: 1dB
+  /// * directFormType - The filter topology. Default direct_form_II
+  void bandPass(
+    int order,
+    double sampleRate,
+    double centerFrequency,
+    double widthFrequency,
+    double rippleDb,
+    [int directFormType = DirectFormAbstract.direct_form_II]
+    ) {
+    _AnalogLowPass analogProto =  _AnalogLowPass(order);
+    analogProto.design(rippleDb);
+    LayoutBase digitalProto =  LayoutBase(order * 2);
+    BandPassTransform(centerFrequency / sampleRate, widthFrequency / sampleRate, digitalProto, analogProto);
+    setLayout(digitalProto, directFormType);
+  }
+}
